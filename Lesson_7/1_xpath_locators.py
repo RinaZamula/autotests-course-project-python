f6827e71f499379119577c729b5bc3a7df97db2b
Марина Замула <m.zamula818@gmail.com>
2025-03-12 15:29:45 +0200

    Урок 7 1. Файл с заданием для курса (написать и сохранить xpath-локаторы в виде кортежей) 2. Файл с проверкой работы поиска элемента по xpath

---
+++ Lesson_7/1_xpath_locators.py
@@ -0,0 +1,14 @@
+#Sign in button
+sign_in_button = ("xpath", "//button[contains(@class, '!whitespace-nowrap')]")
+
+#Header
+header = ("xpath", "//h1[text()='What do you want to do?']")
+
+#Logo
+logo = ("xpath", "(//a[@class='nav-link'])[1]")
+
+#Banner
+banner = ("xpath", "//div[contains(@class, 'banner-discount-timer')]")
+
+#Kotlin Developer Card
+kotlin_developer_card = ("xpath", "//div[@class='card-body']//h5[text()='Kotlin Developer']")
